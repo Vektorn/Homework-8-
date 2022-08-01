40a9a77c7afd8a415ceb82fa37fae663f513840e
Viktor <viktorkyndrat@gmail.com>
2022-08-01 07:40:25 +0300

    Create Shape class

--- src/Homework8/Far.java
+++ src/Homework8/Far.java
@@ -2,13 +2,12 @@ package Homework8;
 
 public class Far {
     public String print (Shape shape){
-        String s = shape.getClass().getName();
         switch (shape.getClass().getName()){
             case "Homework8.Circle": return "Коло";
             case "Homework8.Square": return "Квадрат";
             case "Homework8.Rectangle": return "Прямокутник";
             case "Homework8.EquilateralTriangle": return "Рівньосторонній трикутник";
-            case "Homework8.Rhomb": return "Ромб";
+            case "Homework8.Cylinder": return "Циліндр";
         }
         return null;
     }
